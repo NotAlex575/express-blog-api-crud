43887f87ebe173431d5fe991fcf92fc3c02fd10a
NotAlex575 <dapontealessandro89gmail.com>
2025-08-06 20:14:31 +0200

    esempio uso middleware  errorsHandler (che non è stato pushato precedentemente)

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -5,7 +5,10 @@ const posts = require("../data/postsData.js");
 const index = (req, res) => {
     //EXTRA: FILTER
 
-    Pippo.length();
+    //esempio test middleware errorsHandler (per testare cancellare il commento sotto)
+    //Pippo.length();
+
+    
     //recupero il parametro inserito all'interno di postman (in questo caso recupero un nome inserito)
     const namePost = req.query.name;
 
